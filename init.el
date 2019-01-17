b5eafa456356e2076c1fef2812f330933674b3cd
Yuta Fujita <ofnhwx@komunan.net>
2019-01-17 16:12:00 +0900

    Initial commit.

---
+++ init.el
@@ -0,0 +1,30 @@
+;;; init.el --- load this file at first when emacs was started.
+;;; Commentary:
+;;; Code:
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;;(package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; パス設定
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(let ((emacs-dir (file-name-directory (or load-file-name buffer-file-name))))
+  (setq user-emacs-directory (abbreviate-file-name emacs-dir)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Spacemacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(progn
+  (defvar spacemacs-start-directory
+    (expand-file-name "spacemacs/" user-emacs-directory)
+    "Spacemacs start directory.")
+  (setenv "SPACEMACSDIR" (expand-file-name "init/" user-emacs-directory))
+  (load-file (expand-file-name "init.el" spacemacs-start-directory)))
+
+(provide 'init)
+;;; init.el ends here

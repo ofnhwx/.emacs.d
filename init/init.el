23d6cc1dab0c9cf53708cfa1855c3f87ade6a907
Yuta Fujita <ofnhwx@komunan.net>
2019-01-17 17:03:56 +0900

    update: layer:auto-completion,better-defaultsを有効化

--- init/init.el
+++ init/init.el
@@ -36,11 +36,11 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     helm
-     ;; auto-completion
-     ;; better-defaults
+     auto-completion
+     better-defaults
      emacs-lisp
      git
+     helm
      ;; markdown
      ;; org
      ;; (shell :variables

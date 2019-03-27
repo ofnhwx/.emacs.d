bae5062c1f7c66c41dc96a7ffcb19722fe0da95a
Yuta Fujita <ofnhwx@komunan.net>
2019-03-27 18:49:20 +0900

    update: ヘッダーラインのファイル名をさらに改善(したつもり)

--- custom/user-functions.el
+++ custom/user-functions.el
@@ -7,6 +7,9 @@
     "フォント"
     (list e:font-name :size e:font-size)))
 
+(progn ;; プロジェクトルートの取得
+  (defvar e:project-root-mark '(".git/" ".hg/" ".svn/" ".git")))
+
 (defun e:auth-source-get (property &rest spec)
   "認証情報から SPEC に一致する項目の PROPERTY を取得する."
   (let ((plist (car (apply 'auth-source-search spec)))
@@ -24,20 +27,39 @@
       (cdr list)
     (cons (car list) (e:remove-nth (1- n) (cdr list)))))
 
-(defun e:shorten (filename &optional max)
-  "指定された FILENAME を MAX 以下の長さに短縮する."
-  (let ((filename (abbreviate-file-name filename))
-        (max (or max (window-width)))
-        (target 3)
-        (break nil))
-    (while (and (> (length filename) max)
-                (not break))
-      (let ((parts (split-string filename "/")))
-        (if (> (length parts) (+ target 2))
-            (progn
-              (if (equal (nth target parts) "...")
-                  (setq parts (remove (nth (+ target 1) parts) parts))
-                (setf (nth target parts) "..."))
-              (setq filename (string-join parts "/")))
-          (setq break t))))
-    filename))
+(defun e:project-root (dir)
+  "指定した DIR のプロジェクトルートを返す."
+  (let* ((dir (file-name-directory (expand-file-name dir)))
+         (candidates (mapcar (lambda (name) (locate-dominating-file dir name)) e:project-root-mark))
+         (root (car (sort candidates (lambda (this other) (> (length this) (length other)))))))
+    (if root (abbreviate-file-name root) nil)))
+
+(defun e:project-name (root)
+  "指定した ROOT のプロジェクトの名前をいい感じに返す."
+  (let* ((root (directory-file-name root))
+         (name (file-name-nondirectory root))
+         (parent (e:project-root root)))
+    (if parent
+        (concat (e:project-name parent) "/" name)
+      name)))
+
+(defun e:shorten (path &optional max separator omit)
+  "指定された FILENAME を MAX 以下の長さに短縮する.
+パスの区切り文字は SEPARATOR, 短縮時の省略表示を OMIT で指定する."
+  (let* ((max (or max (window-width)))
+         (separator (or separator "/"))
+         (omit (or omit "..."))
+         (parts (split-string (abbreviate-file-name path) separator))
+         (length (length (abbreviate-file-name path)))
+         (target (if (string-empty-p (car parts)) 3 2))
+         (min-parts (+ target 3)))
+    (when (and (> length max)
+               (> (length parts) min-parts))
+      (setq length (+ length (length omit) (- (length (nth target parts)))))
+      (setf (nth target parts) omit)
+      (setq target (+ target 1))
+      (while (and (> length max)
+                  (> (length parts) min-parts))
+        (setq length (- length (length (nth target parts)) 1))
+        (setq parts (e:remove-nth target parts))))
+    (string-join parts separator)))

77d43bb2981d5535c00fe1d0c3b948fff748bbb9
Yuta Fujita <ofnhwx@komunan.net>
2019-04-05 09:54:37 +0900

    update: which-key の設定を改善

--- custom/user-init.el
+++ custom/user-init.el
@@ -1,3 +1,6 @@
 
 ;; Spacemacs が作成する環境変数を保存しておくファイル
 (set-variable 'spacemacs-env-vars-file (expand-file-name "spacemacs.env" e:private-directory))
+
+;; which-key の設定
+(set-variable 'which-key-enable-extended-define-key t)

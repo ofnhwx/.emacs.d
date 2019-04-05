ec6a19b796b5a1eef743d199b6590268b3bc2077
Yuta Fujita <ofnhwx@komunan.net>
2019-04-05 15:01:01 +0900

    add: evil-plugins を追加

--- custom/layers/github-packages/packages.el
+++ custom/layers/github-packages/packages.el
@@ -1,8 +1,15 @@
 
 (setq github-packages-packages
       '(
+        (evil-plugins :location (recipe :fetcher github :repo "tarao/evil-plugins"))
         (helm-fzf :location (recipe :fetcher github :repo "ofnhwx/helm-fzf"))
         ))
+
+(defun github-packages/init-evil-plugins ()
+  (use-package evil-little-word
+    :after (evil))
+  (use-package evil-textobj-between
+    :after (evil)))
 
 (defun github-packages/init-helm-fzf ()
   (use-package helm-fzf

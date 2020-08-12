4e72ddc4c4f638433cdeca7aa6d4a058cbc87528
Yuta Fujita <ofnhwx@komunan.net>
2020-08-12 18:03:19 +0900

    update: early-init.el の定番を追加

--- early-init.el
+++ early-init.el
@@ -5,4 +5,8 @@
 (let ((dir (file-name-directory (or load-file-name buffer-file-name))))
   (load (expand-file-name "external/spacemacs/early-init" dir)))
 
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
 ;;; early-init.el ends here

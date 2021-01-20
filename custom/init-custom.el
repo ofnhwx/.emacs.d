076cea480cf4c72780c1244ef22d142213bb64ea
Yuta Fujita <ofnhwx@komunan.net>
2021-01-20 15:10:14 +0900

    update: パッケージ関連等大きめの差分を custom/init.el から分離

---
+++ custom/init-custom.el
@@ -0,0 +1,136 @@
+
+(defvar custom-configuration-layers nil)
+(setq custom-configuration-layers
+      '(
+        ;; +checkers
+        spell-checking
+        syntax-checking
+        ;; +completion
+        (auto-completion :variables
+                         auto-completion-enable-help-tooltip nil
+                         auto-completion-enable-sort-by-usage t
+                         auto-completion-use-company-box t)
+        helm
+        ;; +emacs
+        better-defaults
+        org
+        ;; +email
+        notmuch
+        ;; +filetree
+        treemacs
+        ;; +frameworks
+        react
+        ruby-on-rails
+        ;; +fun
+        emoji
+        ;; +intl
+        japanese
+        ;; +lang
+        csv
+        emacs-lisp
+        html
+        (javascript :variables
+                    javascript-backend 'lsp)
+        markdown
+        (php :variables
+             php-backend 'lsp)
+        (python :variables
+                python-backend 'lsp)
+        (ruby :variables
+              ruby-backend 'lsp
+              ruby-enable-enh-ruby-mode nil
+              ruby-test-runner 'rspec
+              ruby-highlight-debugger-keywords t)
+        shell-scripts
+        sql
+        vimscript
+        yaml
+        ;; +misc
+        copy-as-format
+        dtrt-indent
+        multiple-cursors
+        ;; +source-control
+        (git :variables
+             git-magit-status-fullscreen t
+             git-enable-magit-delta-plugin t)
+        github
+        ;; +tags
+        gtags
+        ;; +tools
+        docker
+        lsp
+        nginx
+        prodigy
+        restclient
+        (shell :variables
+               shell-default-height 60
+               shell-default-shell 'vterm)
+        vagrant
+        ;; +web-services
+        search-engine
+        ))
+
+(defvar custom-additional-packages nil)
+(setq custom-additional-packages
+      '(
+        (helm-fzf     :location (recipe :fetcher github :repo "ofnhwx/helm-fzf"))
+        (locale-eaw   :location (recipe :fetcher github :repo "hamano/locale-eaw"))
+        atomic-chrome
+        codic
+        company-tabnine
+        company-try-hard
+        deadgrep
+        dired-filter
+        dired-toggle-sudo
+        elisp-demos
+        evil-easymotion
+        evil-owl
+        foreman-mode
+        grugru
+        helm-tramp
+        helpful
+        leaf
+        leaf-tree
+        magit-libgit
+        ox-reveal
+        psysh
+        visual-regexp
+        vlf
+        ))
+
+(defvar custom-excluded-packages nil)
+(setq custom-excluded-packages
+      '(
+        ;; +spacemacs/spacemacs-editing
+        dired-quick-sort
+        ;; +spacemacs/spacemacs-evil
+        evil-escape
+        ;; +checkers/syntax-checking
+        flycheck-pos-tip
+        ;; +intl/japanese
+        pangu-spacing
+        ;; +lang/php
+        company-php
+        ))
+
+(defun custom-user-init ()
+  ;; +distributions/spacemacs-bootstrap
+  (set-variable 'vim-style-remap-Y-to-y$ t)
+  ;; +spacemacs/spacemacs-completion
+  (set-variable 'helm-use-fuzzy nil)
+  ;; その他、先に設定しておきたいもの
+  (set-variable 'custom-file null-device)
+  (set-variable 'spacemacs-env-vars-file (expand-file-name "spacemacs.env" spacemacs-cache-directory))
+  (set-variable 'viper-mode nil)
+  (set-variable 'which-key-enable-extended-define-key t)
+  (set-variable 'which-key-show-early-on-C-h t)
+  (setq-default ispell-local-dictionary "en_US")
+  ;; yasnippet で余計なものを読込ませないための対策
+  (defvar e:yas-snippet-dirs (list (expand-file-name "snippets" e:custom-directory)))
+  (dolist (dir e:yas-snippet-dirs)
+    (unless (file-exists-p dir)
+      (make-directory dir)))
+  (with-eval-after-load "yasnippet"
+    (define-advice yas-reload-all (:around (fn &rest args) only-custom-snippets)
+      (when (equal yas-snippet-dirs e:yas-snippet-dirs)
+        (funcall fn args)))))
--- custom/init.el
+++ custom/init.el
@@ -2,6 +2,8 @@
 ;; This file is loaded by Spacemacs at startup.
 ;; It must be stored in your home directory.
 
+(load (expand-file-name "init-custom" e:custom-directory))
+
 (defun dotspacemacs/layers ()
   "Layer configuration:
 This function should only modify configuration layer settings."
@@ -31,74 +33,7 @@ This function should only modify configuration layer settings."
    dotspacemacs-configuration-layer-path '()
 
    ;; List of configuration layers to load.
-   dotspacemacs-configuration-layers
-   '(
-     ;; +checkers
-     spell-checking
-     syntax-checking
-     ;; +completion
-     (auto-completion :variables
-                      auto-completion-enable-help-tooltip nil
-                      auto-completion-enable-sort-by-usage t
-                      auto-completion-use-company-box t)
-     helm
-     ;; +emacs
-     better-defaults
-     org
-     ;; +email
-     notmuch
-     ;; +filetree
-     treemacs
-     ;; +frameworks
-     react
-     ruby-on-rails
-     ;; +fun
-     emoji
-     ;; +intl
-     japanese
-     ;; +lang
-     csv
-     emacs-lisp
-     html
-     (javascript :variables
-                 javascript-backend 'lsp)
-     markdown
-     (php :variables
-          php-backend 'lsp)
-     (ruby :variables
-           ruby-backend 'lsp
-           ruby-enable-enh-ruby-mode nil
-           ruby-test-runner 'rspec
-           ruby-highlight-debugger-keywords t)
-     shell-scripts
-     sql
-     vimscript
-     yaml
-     ;; +misc
-     copy-as-format
-     dtrt-indent
-     multiple-cursors
-     ;; +source-control
-     (git :variables
-          git-magit-status-fullscreen t
-          git-enable-magit-delta-plugin t)
-     github
-     ;; +tags
-     gtags
-     ;; +tools
-     docker
-     lsp
-     nginx
-     prodigy
-     restclient
-     (shell :variables
-            shell-default-height 60
-            shell-default-shell 'vterm)
-     vagrant
-     ;; +web-services
-     search-engine
-     )
-
+   dotspacemacs-configuration-layers custom-configuration-layers
 
    ;; List of additional packages that will be installed without being wrapped
    ;; in a layer (generally the packages are installed only and should still be
@@ -108,50 +43,13 @@ This function should only modify configuration layer settings."
    ;; `dotspacemacs/user-config'. To use a local version of a package, use the
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages
-   '(
-     (helm-fzf     :location (recipe :fetcher github :repo "ofnhwx/helm-fzf"))
-     (locale-eaw   :location (recipe :fetcher github :repo "hamano/locale-eaw"))
-     atomic-chrome
-     codic
-     company-tabnine
-     company-try-hard
-     deadgrep
-     dired-filter
-     dired-toggle-sudo
-     elisp-demos
-     evil-easymotion
-     evil-owl
-     foreman-mode
-     grugru
-     helm-tramp
-     helpful
-     leaf
-     leaf-tree
-     magit-libgit
-     ox-reveal
-     psysh
-     visual-regexp
-     vlf
-     )
+   dotspacemacs-additional-packages custom-additional-packages
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
 
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages
-   '(
-     ;; +spacemacs/spacemacs-editing
-     dired-quick-sort
-     ;; +spacemacs/spacemacs-evil
-     evil-escape
-     ;; +checkers/syntax-checking
-     flycheck-pos-tip
-     ;; +intl/japanese
-     pangu-spacing
-     ;; +lang/php
-     company-php
-     )
+   dotspacemacs-excluded-packages custom-excluded-packages
 
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
@@ -581,27 +479,7 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
-  ;; +distributions/spacemacs-bootstrap
-  (set-variable 'vim-style-remap-Y-to-y$ t)
-  ;; +spacemacs/spacemacs-completion
-  (set-variable 'helm-use-fuzzy nil)
-  ;; その他、先に設定しておきたいもの
-  (set-variable 'custom-file null-device)
-  (set-variable 'spacemacs-env-vars-file (expand-file-name "spacemacs.env" spacemacs-cache-directory))
-  (set-variable 'viper-mode nil)
-  (set-variable 'which-key-enable-extended-define-key t)
-  (set-variable 'which-key-show-early-on-C-h t)
-  (setq-default ispell-local-dictionary "en_US")
-  ;; yasnippet で余計なものを読込ませないための対策
-  (defvar e:yas-snippet-dirs (list (expand-file-name "snippets" e:custom-directory)))
-  (dolist (dir e:yas-snippet-dirs)
-    (unless (file-exists-p dir)
-      (make-directory dir)))
-  (with-eval-after-load "yasnippet"
-    (define-advice yas-reload-all (:around (fn &rest args) only-custom-snippets)
-      (when (equal yas-snippet-dirs e:yas-snippet-dirs)
-        (funcall fn args))))
-  )
+  (custom-user-init))
 
 (defun dotspacemacs/user-load ()
   "Library to load while dumping.

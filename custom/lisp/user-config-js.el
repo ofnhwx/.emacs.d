b42172d8e4bd291b1d0f2020f1391ecf8bf29ecf
Yuta Fujita <ofnhwx@komunan.net>
2021-02-07 21:52:30 +0900

    feat: tsx 用の設定を追加

--- custom/lisp/user-config-js.el
+++ custom/lisp/user-config-js.el
@@ -1,5 +1,14 @@
 ;;; user-config-js.el
 
+(leaf web-mode ;; typescript-tsx-mode
+  :hook (typescript-tsx-mode-hook . setup-typescript-tsx-mode)
+  :config
+  (defun setup-typescript-tsx-mode ()
+    (set-variable 'web-mode-code-indent-offset 2)
+    (set-variable 'web-mode-css-indent-offset 2)
+    (set-variable 'web-mode-markup-indent-offset 2)
+    (set-variable 'web-mode-sql-indent-offset 2)))
+
 (leaf js2-mode
   :defer-config
   (set-face-attribute 'js2-external-variable nil :foreground "#ff0000" :underline t)

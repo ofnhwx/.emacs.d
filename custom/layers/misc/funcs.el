178c5c2ce51eb5cc0f486306752493c9426bd1be
Yuta Fujita <ofnhwx@komunan.net>
2021-02-16 08:16:34 +0900

    refactor: 不要となった定義を削除

--- custom/layers/misc/funcs.el
+++ custom/layers/misc/funcs.el
@@ -7,12 +7,6 @@
     (when plist
       (plist-get plist pkey))))
 
-(defun e:remove-nth (n list)
-  "N 番目の要素を LIST から取り除いて返す."
-  (if (or (zerop n) (null list))
-      (cdr list)
-    (cons (car list) (e:remove-nth (1- n) (cdr list)))))
-
 (defun e:toggle-indent-tabs-mode ()
   "インデントモードをタブ/空白で切替える."
   (interactive)

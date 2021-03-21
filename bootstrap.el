d23babd4477d990df3cedf5984d9886d8644e55d
Yuta Fujita <ofnhwx@komunan.net>
2021-03-21 22:09:40 +0900

    fix: パスの表記を統一

--- bootstrap.el
+++ bootstrap.el
@@ -7,9 +7,9 @@
 (let ((base (file-name-directory (or load-file-name buffer-file-name))))
   (setq user-emacs-directory (abbreviate-file-name base)))
 
-(defconst e:external-directory      (expand-file-name "external/"     user-emacs-directory))
-(defconst e:private-directory       (expand-file-name "private/"      user-emacs-directory))
-(defconst e:distributions-directory (expand-file-name "distributions" user-emacs-directory))
+(defconst e:external-directory      (expand-file-name "external/"      user-emacs-directory))
+(defconst e:private-directory       (expand-file-name "private/"       user-emacs-directory))
+(defconst e:distributions-directory (expand-file-name "distributions/" user-emacs-directory))
 (defconst e:distribution-directory  (expand-file-name e:distribution e:distributions-directory))
 
 (add-to-list 'load-path (expand-file-name "lisp" e:distribution-directory))

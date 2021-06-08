9e071e10c3cc2578eb1713c519318c36d72b0f0a
Yuta Fujita <ofnhwx@komunan.net>
2021-06-08 14:17:51 +0900

    refactor: 自分用のレイヤーは速度重視でいいのでちょっと調整

--- distributions/spacemacs/layers/misc/packages.el
+++ distributions/spacemacs/layers/misc/packages.el
@@ -29,160 +29,118 @@
     ))
 
 (defun misc/init-atomic-chrome ()
-  (use-package atomic-chrome
-    :config
-    (spacemacs/defer-until-after-user-config #'atomic-chrome-start-server)))
+  (spacemacs/defer-until-after-user-config #'atomic-chrome-start-server))
 
 (defun misc/init-beacon ()
-  (use-package beacon
-    :spacediminish beacon-mode
-    :config
-    (beacon-mode 1)))
+  (with-eval-after-load 'beacon
+    (spacemacs|hide-lighter beacon-mode))
+  (spacemacs/defer-until-after-user-config #'beacon-mode))
 
-(defun misc/init-codic ()
-  (use-package codic
-    :no-require t))
+(defun misc/init-codic ())
 
 (defun misc/init-company-org-block ()
-  (with-no-warnings
-    (spacemacs|add-company-backends :backends company-org-block :modes org-mode)))
+  (with-no-warnings (spacemacs|add-company-backends :backends company-org-block :modes org-mode)))
 
 (defun misc/init-company-prescient ()
-  (use-package company-prescient
-    :after company
-    :config
+  (with-eval-after-load 'company
     (company-prescient-mode 1)))
 
 (defun misc/init-company-tabnine ()
-  (use-package company-tabnine
-    :defer t
-    :init
+  (with-eval-after-load 'company-tabnine
     (set-variable 'company-tabnine-binaries-folder
                   (expand-file-name "tabnine" spacemacs-cache-directory))))
 
 (defun misc/init-company-try-hard ()
-  (use-package company-try-hard
-    :bind (("C-z" . company-try-hard)
-           :map company-active-map
-           ("C-z" . company-try-hard))))
+  (bind-keys :package company-try-hard
+             ("C-z" . company-try-hard)
+             :map company-active-map
+             ("C-z" . company-try-hard)))
 
 (defun misc/init-cov ()
-  (use-package cov
-    :defer t
-    :spacediminish (cov-mode " ☂" " COV")
-    :init
+  (with-eval-after-load 'cov
+    (spacemacs|diminish cov-mode " ☂" " COV")
     (set-variable 'cov-coverage-file-paths '(cov--locate-simplecov))
     (set-variable 'cov-coverage-mode t)
-    :config
-    (defun cov--locate-simplecov (file-dir file-name)
-      (let ((dir (kllib:project-root file-dir)))
-        (when dir
-          (cons (f-expand "coverage/.resultset.json" dir) 'simplecov))))
-    (defun cov--simplecov-parse ()
-      (let* ((contents (buffer-string))
-             (coverage (let-alist (json-parse-string contents :object-type 'alist :array-type 'list)
-                         .RSpec.coverage))
-             (project-root (f-expand (kllib:project-root cov-coverage-file))))
-        (-map (lambda (item)
-                (let ((file (symbol-name (first item)))
-                      (list (cdadr item)))
-                  (cons (s-replace project-root "" file)
-                        (->> list
-                          (--map-indexed (list (1+ it-index) it))
-                          (--reject (eq (second it) :null))))))
-              coverage)))))
+    (prog1 "`simplecov' 用の設定"
+      (defun cov--locate-simplecov (file-dir file-name)
+        (let ((dir (kllib:project-root file-dir)))
+          (when dir
+            (cons (f-expand "coverage/.resultset.json" dir) 'simplecov))))
+      (defun cov--simplecov-parse ()
+        (let* ((contents (buffer-string))
+               (coverage (let-alist (json-parse-string contents :object-type 'alist :array-type 'list)
+                           .RSpec.coverage))
+               (project-root (f-expand (kllib:project-root cov-coverage-file))))
+          (-map (lambda (item)
+                  (let ((file (symbol-name (first item)))
+                        (list (cdadr item)))
+                    (cons (s-replace project-root "" file)
+                          (->> list
+                            (--map-indexed (list (1+ it-index) it))
+                            (--reject (eq (second it) :null))))))
+                coverage))))))
 
-(defun misc/init-deadgrep ()
-  (use-package deadgrep
-    :no-require))
+(defun misc/init-deadgrep ())
 
 (defun misc/init-dired-filter ()
-  (use-package dired-filter
-    :hook (dired-mode . dired-filter-mode)))
+  (add-hook 'dired-mode-hook #'dired-filter-mode))
 
-(defun misc/init-dired-toggle-sudo ()
-  (use-package dired-toggle-sudo
-    :no-require t))
+(defun misc/init-dired-toggle-sudo ())
 
 (defun misc/init-elisp-demos ()
-  (use-package elisp-demos
-    :defer t
-    :init
-    (advice-add 'describe-function-1 :after #'elisp-demos-advice-describe-function-1)
-    (advice-add 'helpful-update      :after #'elisp-demos-advice-helpful-update)))
+  (advice-add 'describe-function-1 :after #'elisp-demos-advice-describe-function-1)
+  (advice-add 'helpful-update      :after #'elisp-demos-advice-helpful-update))
 
 (defun misc/init-evil-owl ()
-  (use-package evil-owl
-    :spacediminish evil-owl-mode
-    :config
-    (evil-owl-mode 1)))
+  (with-eval-after-load 'evil-owl
+    (spacemacs|hide-lighter evil-owl-mode))
+  (spacemacs/defer-until-after-user-config #'evil-owl-mode))
 
 (defun misc/init-foreman-mode ()
-  (use-package foreman-mode
-    :bind (:map foreman-mode-map
-                ("R" . foreman-restart)
-                ("S" . foreman-start)
-                ("X" . foreman-stop)
-                ("x" . foreman-kill-proc))
-    :init
-    (spacemacs/set-leader-keys "atf" #'foreman)))
+  (spacemacs/set-leader-keys "atf" #'foreman)
+  (bind-keys :package foreman-mode
+             :map foreman-mode-map
+             ("R" . foreman-restart)
+             ("S" . foreman-start)
+             ("X" . foreman-stop)
+             ("x" . foreman-kill-proc)))
 
 (defun misc/init-good-scroll ()
   (spacemacs/defer-until-after-user-config #'good-scroll-mode))
 
 (defun misc/init-grugru ()
-  (use-package grugru
-    :defer t
-    :init
-    (spacemacs/set-leader-keys "xx" #'grugru)))
+  (spacemacs/set-leader-keys "xgg" #'grugru))
 
 (defun misc/init-helpful ()
-  (use-package helpful
-    :defer t
-    :init
-    (spacemacs/declare-prefix "hdd" "helpful")
-    (spacemacs/set-leader-keys
-      "hddc" 'helpful-callable
-      "hddd" 'helpful-at-point
-      "hddf" 'helpful-function
-      "hddi" 'helpful-command
-      "hddk" 'helpful-key
-      "hddm" 'helpful-macro
-      "hdds" 'helpful-symbol
-      "hddv" 'helpful-variable)))
+  (spacemacs/declare-prefix "hdd" "helpful")
+  (spacemacs/set-leader-keys
+    "hddc" 'helpful-callable
+    "hddd" 'helpful-at-point
+    "hddf" 'helpful-function
+    "hddi" 'helpful-command
+    "hddk" 'helpful-key
+    "hddm" 'helpful-macro
+    "hdds" 'helpful-symbol
+    "hddv" 'helpful-variable))
 
-(defun misc/init-leaf ()
-  (use-package leaf
-    :no-require t))
+(defun misc/init-leaf ())
 
 (defun misc/init-magit-libgit ()
-  (use-package magit-libgit
-    :after (magit)
-    :config
+  (with-eval-after-load 'magit
     (libgit-load)))
 
 (defun misc/init-ob-typescript ()
   (spacemacs|use-package-add-hook org
     :post-config
-    (use-package ob-typescript
-      :init
-      (add-to-list 'org-babel-load-languages '(typescript . t)))))
+    (add-to-list 'org-babel-load-languages '(typescript . t))))
 
 (defun misc/init-ox-reveal ()
-  (use-package ox-reveal
-    :defer t
-    :init
-    (set-variable 'org-reveal-reveal-js-version 4)
-    (set-variable 'org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js")))
+  (set-variable 'org-reveal-reveal-js-version 4)
+  (set-variable 'org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"))
 
-(defun misc/init-psysh ()
-  (use-package psysh
-    :no-require t))
+(defun misc/init-psysh ())
 
-(defun misc/init-vlf ()
-  (use-package vlf
-    :no-require t))
+(defun misc/init-vlf ())
 
 (defun misc/init-visual-regexp ()
-  (use-package visual-regexp
-    :bind (([remap query-replace] . vr/query-replace))))
+  (bind-key [remap query-replace] #'vr/query-replace))

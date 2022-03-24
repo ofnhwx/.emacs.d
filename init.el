a102b044ce58c45c21d52b4d3edaf1ee5f45ae16
Yuta Fujita <ofnhwx@komunan.net>
2022-03-24 09:24:36 +0900

    chore: 余計な警告が出ないように

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 ;;(package-initialize)
 
 ;; https://emacs-jp.slack.com/archives/C1B5WTJLQ/p1614558372038400
-(when (string-equal emacs-version "28.0.91")
+(when (string-equal emacs-version "28.0.92")
   (set-variable 'warning-suppress-log-types '((comp)))
   (set-variable 'warning-suppress-types '((comp))))
 

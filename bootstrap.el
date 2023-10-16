8345802400cb3e2393ca6f0fbe5fecdb3e3a6186
Yuta Fujita <ofnhwx@komunan.net>
2023-10-16 13:21:01 +0900

    起動処理の改善

--- bootstrap.el
+++ bootstrap.el
@@ -2,17 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar e:distribution (or (getenv "EMACS_DISTRIBUTION") "nsmacs"))
-
-(let ((base (file-name-directory (or load-file-name buffer-file-name))))
-  (setq user-emacs-directory (abbreviate-file-name base)))
-
-(defconst e:external-directory      (expand-file-name "external/"      user-emacs-directory))
-(defconst e:private-directory       (expand-file-name "private/"       user-emacs-directory))
-(defconst e:distributions-directory (expand-file-name "distributions/" user-emacs-directory))
-(defconst e:distribution-directory  (expand-file-name e:distribution e:distributions-directory))
-
-(add-to-list 'load-path (expand-file-name "lisp" e:distribution-directory))
+(unless (boundp 'e:distribution)
+  (defvar e:distribution (or (getenv "EMACS_DISTRIBUTION") "nsmacs"))
+  (let ((base (abbreviate-file-name (file-name-directory (or load-file-name buffer-file-name)))))
+    (setq user-emacs-directory base)
+    (defconst e:external-directory      (expand-file-name "external/"      user-emacs-directory))
+    (defconst e:private-directory       (expand-file-name "private/"       user-emacs-directory))
+    (defconst e:distributions-directory (expand-file-name "distributions/" user-emacs-directory))
+    (defconst e:distribution-directory  (expand-file-name e:distribution e:distributions-directory)))
+  (add-to-list 'load-path (expand-file-name "lisp" e:distribution-directory)))
 
 (load (expand-file-name "bootstrap" e:distribution-directory))
 
